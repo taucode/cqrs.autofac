103718b44d4fa49413177442b4e12446670f9036
Andrey Kovalenko <ak@taucode.com>
2019-06-25 00:59:32 +0300

    ready to deploy

---
+++ src/TauCode.Cqrs.Autofac/AutofacCommandHandlerFactory.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using TauCode.Cqrs.Commands;
+
+namespace TauCode.Cqrs.Autofac
+{
+    public class AutofacCommandHandlerFactory : ICommandHandlerFactory
+    {
+        private readonly ILifetimeScope _lifetimeScope;
+
+        public AutofacCommandHandlerFactory(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+
+        public ICommandHandler<TCommand> Create<TCommand>() where TCommand : ICommand
+        {
+            return _lifetimeScope.Resolve<ICommandHandler<TCommand>>();
+        }
+    }
+}
