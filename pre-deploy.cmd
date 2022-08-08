682d7f4d5e9aa6ef959bcb0b42edf1efd7160630
Andrey Kovalenko <ak@taucode.com>
2022-08-08 16:33:41 +0300

    Updated content for 1.5.0.

--- pre-deploy.cmd
+++ pre-deploy.cmd
@@ -3,7 +3,7 @@ dotnet restore
 dotnet build --configuration Debug
 dotnet build --configuration Release
 
-dotnet test -c Debug .\tests\TauCode.Cqrs.Autofac.Tests\TauCode.Cqrs.Autofac.Tests.csproj
-dotnet test -c Release .\tests\TauCode.Cqrs.Autofac.Tests\TauCode.Cqrs.Autofac.Tests.csproj
+dotnet test -c Debug .\test\TauCode.Cqrs.Autofac.Tests\TauCode.Cqrs.Autofac.Tests.csproj
+dotnet test -c Release .\test\TauCode.Cqrs.Autofac.Tests\TauCode.Cqrs.Autofac.Tests.csproj
 
 nuget pack nuget\TauCode.Cqrs.Autofac.nuspec
